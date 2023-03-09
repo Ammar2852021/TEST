1945456890ca241e20755f152339a2fb74846203
Ammar Ahmed <114263245+Ammar2852021@users.noreply.github.com>
2023-03-09 17:37:13 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//  START START PAGE THIS CODE HIDEN TITEL NAME IN 1.5 S
+let start =  document.getElementById('start');
+setTimeout( function(){start.style.display='none'},3000 );
+//  END START PAGE 
+let LOGIN = document.getElementById('LOGIN');
+setTimeout( function(){LOGIN.style.display='block'},3000 );
